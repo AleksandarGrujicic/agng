1681ef199d248fe168a1ac0b34ba0e417ba1e8aa
Aleksandar Grujicic <aleksandargrujicic86@gmail.com>
2026-02-22 21:02:45 +0100

    fix: hash routing + relative logo path for GitHub Pages

--- apps/finance/src/app/app.config.ts
+++ apps/finance/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideTransloco } from '@jsverse/transloco';
 import { APP_ROUTES } from './app.routes';
 import { TranslocoHttpLoader } from './transloco-loader';
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideAnimations(),
     provideHttpClient(withFetch()),
-    provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES, withHashLocation()),
     provideTransloco({
       config: {
         availableLangs: ['en', 'sr'],
